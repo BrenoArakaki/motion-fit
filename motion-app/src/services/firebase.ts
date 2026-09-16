51c0d9f30faa9704529a62dbc68551e71eaf1e61
Breno Arakaki <brenoarakaki17@gmail.com>
2026-09-15 22:00:38 -0300

    fix: .env

--- motion-app/src/services/firebase.ts
+++ motion-app/src/services/firebase.ts
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAM22hGJJvSNI0HXax04klpATw6-0QUtHU",
-  authDomain: "motion-app-58dbb.firebaseapp.com",
-  projectId: "motion-app-58dbb",
-  storageBucket: "motion-app-58dbb.firebasestorage.app",
-  messagingSenderId: "614191969517",
-  appId: "1:614191969517:web:fd4af46c2ac07f9d1408e4",
-  measurementId: "G-SP1BV3JQXT"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
